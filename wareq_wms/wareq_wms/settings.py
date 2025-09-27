481a0b7c4b706342076d06dddf926fad9f2dbb71
Jacob Qumsiyeh <qumsiyeh37@gmail.com>
2025-09-27 22:57:01 +0300

    Fixing Static files

--- wareq_wms/wareq_wms/settings.py
+++ wareq_wms/wareq_wms/settings.py
@@ -213,7 +213,7 @@ USE_TZ = True
 # ========================
 # STATIC & MEDIA
 # ========================
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
