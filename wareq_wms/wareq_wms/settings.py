16176c03bab4e670ad182a8eeff11855076d7acc
Jacob Qumsiyeh <qumsiyeh37@gmail.com>
2025-09-27 13:20:32 +0300

    Fixing the depolyment

--- wareq_wms/wareq_wms/settings.py
+++ wareq_wms/wareq_wms/settings.py
@@ -168,17 +168,15 @@ WSGI_APPLICATION = 'wareq_wms.wsgi.application'
 # ========================
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv("DB_ENGINE", "django.db.backends.mysql"),
-        'NAME': os.getenv("DB_NAME", "wareq_wms"),
-        'USER': os.getenv("DB_USER", "root"),
-        'PASSWORD': os.getenv("DB_PASSWORD", ""),
-        'HOST': os.getenv("DB_HOST", "localhost"),
-        'PORT': os.getenv("DB_PORT", "3306"),
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-        }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'wareqwms-db',   # <-- must match DB name in AWS console
+        'USER': 'admin',         # <-- must match master username
+        'PASSWORD': '190275@11Jqq',
+        'HOST': 'wareqwms-db.cbc4i2gqy5z8.eu-north-1.rds.amazonaws.com',
+        'PORT': '3306',
     }
 }
+
 
 
 # Password validation
